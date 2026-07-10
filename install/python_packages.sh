038826fbf6278b892f30e16383e773560ad1c33b
Kevin Pedro <kpedro88@gmail.com>
2026-07-10 17:08:32 -0500

    use lcg-venv

--- install/python_packages.sh
+++ install/python_packages.sh
@@ -3,13 +3,23 @@
 mkdir -p python_packages
 cd python_packages
 
+wget https://github.com/kpedro88/lcg-venv/raw/refs/heads/main/lcg-venv
+chmod +x lcg-venv
+./lcg-venv mbenv
+
+cat << 'EOF' > mb_init.sh
+source ${MODEL_BUILDING}/install/python_packages/mbenv/bin/activate
+EOF
+
+source mb_init.sh
+
 PKGS_UPGRADE=(
 coffea==2025.12.0 \
 mplhep \
 )
 
 for PKG in ${PKGS_UPGRADE[@]}; do
-	HOME=$PWD pip install --user --no-cache-dir --upgrade $PKG
+	pip install --upgrade $PKG
 done
 
 PKGS=(
@@ -18,9 +28,5 @@ fastjet \
 )
 
 for PKG in ${PKGS[@]}; do
-	HOME=$PWD pip install --user --no-cache-dir $PKG
+	pip install $PKG
 done
-
-cat << 'EOF' > mb_init.sh
-export PYTHONPATH=${MODEL_BUILDING}/install/python_packages/.local/lib/python3.11/site-packages/:${PYTHONPATH}
-EOF
