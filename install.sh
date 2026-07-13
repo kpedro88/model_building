ee9f06fe562aa051a81a1c8c331eac5e126202a1
Kevin Pedro <kpedro88@gmail.com>
2026-07-13 12:22:43 -0500

    allow installing different pythia versions and handle key differences

--- install.sh
+++ install.sh
@@ -2,6 +2,13 @@
 
 source init.sh
 
+while getopts "p:" opt; do
+	case "$opt" in
+		p) export PYTHIA8MINOR="$OPTARG"
+		;;
+	esac
+done
+
 cd install
 
 for EXTERNAL in $MODEL_BUILDING_EXTERNALS; do
--- install/pythia8.sh
+++ install/pythia8.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-PYTHIA_VERSION=pythia8310
+if [ -z "PYTHIA8MINOR" ]; then
+	export PYTHIA8MINOR=10
+fi
+
+PYTHIA_VERSION=pythia83${PYTHIA8MINOR}
+PYTHIA_EXE=main42
+if [ "$PYTHIA8MINOR" -ge 12 ]; then
+	PYTHIA_EXE=main132
+fi
 
 wget "https://pythia.org/releases/pythia83/${PYTHIA_VERSION}.tgz"
 tar -xzf ${PYTHIA_VERSION}.tgz
@@ -16,7 +24,7 @@ make -j 8
 make install
 
 (cd examples
-make main42
+make ${PYTHIA_EXE}
 )
 
 cat << 'EOF' > mb_init.sh
@@ -24,5 +32,6 @@ export PYTHIA8=${MODEL_BUILDING}/install/pythia8
 export PATH=${PYTHIA8}/bin/:${PATH}
 export LD_LIBRARY_PATH=${PYTHIA8}/lib/:${LD_LIBRARY_PATH}
 export PYTHIA8DATA=$(pythia8-config --xmldoc)
-export PYTHIA8RUNNER=${PYTHIA8}/examples/main42
+export PYTHIA8MINOR=${PYTHIA8MINOR}
+export PYTHIA8RUNNER=${PYTHIA8}/examples/${PYTHIA_EXE}
 EOF
